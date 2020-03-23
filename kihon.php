b0c1de4d618723997dd37dc622a71ee5c092fa2b
tosswi <Heckel3437@gmail.com>
2020-03-23 23:58:03 +0900

    first PHP

---
+++ kihon.php
@@ -0,0 +1,11 @@
+<?php
+print('PHPを勉強中です！');
+print('PHPも勉強中です！');
+print('PHPで勉強中です！');
+print('PHPで勉強中です！');
+?>
+<?php
+print('iiii');
+?>
+<p><?php print('phpを勉強中です！！'); ?></p>
+<a href="<?php print('http://h2o-space.com'); ?>">タグの属性にPHPを生みこみました。</a>
